af72826fe0365d8c0f46167cc0f5b271f6b6f231
lostcatbox <lostcatbox@gmail.com>
2025-05-10 15:30:28 +0900

    [FIX] 캐시 전략 지식 정리 및 인기 상품 캐싱 전략 Redis로 사용변경

--- src/main/kotlin/com/example/server/config/CacheConfig.kt
+++ src/main/kotlin/com/example/server/config/CacheConfig.kt
@@ -2,15 +2,30 @@ package com.example.server.config
 
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
 
 @Configuration
 @EnableCaching
 class CacheConfig {
     @Bean
-    fun cacheManager(): CacheManager {
-        return ConcurrentMapCacheManager("popularProducts")
+    fun cacheManager(connectionFactory: RedisConnectionFactory): CacheManager {
+        val cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(5)) // 캐시 만료 시간 5분
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
+            .disableCachingNullValues()
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(connectionFactory)
+            .cacheDefaults(cacheConfig)
+            .build()
     }
 } 
